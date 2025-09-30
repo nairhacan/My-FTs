2e434a2e9fd6435d6f5ebd795e3816d30b02ceac
Kina`SaQina <nairhacan64@gmail.com>
2025-09-30 20:06:26 +0700

    Add files via upload

---
+++ backend/controllers/dataContoller.js
@@ -0,0 +1,11 @@
+
+// syn models
+
+// const dataModel = require("../models/dataModel");
+
+// async function getAllData() { return await dataModel.getAll(); }
+// async function addData(item) { return await dataModel.insert(item); }
+// async function updateData(item) { return await dataModel.update(item); }
+// async function deleteData(id) { return await dataModel.remove(id); }
+
+// module.exports = { getAllData, addData, updateData, deleteData };
---
+++ backend/models/dataModel.js
@@ -0,0 +1,11 @@
+
+//syn Models
+
+// const db = require("../services/db");
+
+// function getAll() { return db.all("SELECT * FROM items"); }
+// function insert(item) { return db.run("INSERT INTO items (title, content) VALUES (?, ?)", [item.title, item.content]); }
+// function update(item) { return db.run("UPDATE items SET title = ?, content = ? WHERE id = ?", [item.title, item.content, item.id]); }
+// function remove(id) { return db.run("DELETE FROM items WHERE id = ?", [id]); }
+
+// module.exports = { getAll, insert, update, remove };
